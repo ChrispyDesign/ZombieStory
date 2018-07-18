0f344fd5eee4bfd51756e0bc0c2df5f24cc8c7b9
Christopher Poermandya <33267320+ChrispyDesign@users.noreply.github.com>
2018-07-18 16:46:24 +1000

    Initial commit.

---
+++ Zombie Story - v3.0/Assets/Scripts/RaisePlayerHeight.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaisePlayerHeight : MonoBehaviour {
+
+    //private float defaultHeight = 0.0f;                 // the default heigh of the player when they are standing up on their feet
+    private Vector3 standingHeight = new Vector3 (0,0,0); // Vector3 for the player's standing height
+    public float damping = 5.0f;                          // variable to smooth the transition
+
+    #region Raise Player's Height
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            standingHeight = new Vector3(other.transform.position.x, 0, other.transform.position.z);
+            other.transform.position = Vector3.Lerp(transform.position, standingHeight, Time.deltaTime * damping);
+        }
+    }
+    #endregion
+}
