2128b4069b178b1e0861607bf919e312b61a7f72
Christopher Poermandya <33267320+ChrispyDesign@users.noreply.github.com>
2018-07-20 08:39:03 +1000

    Camera Zoom scripts and test

---
+++ Zombie Story - v3.0/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraZoom : MonoBehaviour {
+
+    public Camera myCamera;                 // sets the camera game object
+    public float maxZoom;                   // sets the minimum value of the FOV to zoom to
+    public float zoomSpeed;                 // sets how fast the camera changes to the minimum FOV
+
+    private float defaultZoom;              // the beginning, default zoom of the camera
+    private float targetZoom;               // sets the point where the camera is lerping to and from
+
+	void Start () {
+        defaultZoom = myCamera.fieldOfView; // sets the defaultZoom to be the camera's field of view
+	}
+	
+	void Update () {
+        if (Input.GetMouseButton(1))
+        {
+            targetZoom = maxZoom;
+        }
+        else
+        {
+            targetZoom = defaultZoom;
+        }
+
+        if (myCamera.fieldOfView != targetZoom)
+        {
+            Debug.Log("zoomin");
+            myCamera.fieldOfView = Mathf.Lerp(myCamera.fieldOfView, targetZoom, Time.deltaTime * zoomSpeed);
+        }
+	}
+}
