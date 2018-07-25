d3c9b30d7b0564ea43f001346946503e351cb060
Christopher Poermandya <33267320+ChrispyDesign@users.noreply.github.com>
2018-07-25 17:43:00 +1000

    added all the SFX. New scene for the Alpha, with lots of changes.

---
+++ Zombie Story - v3.0/Assets/Scripts/CursorVisible.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorVisible : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Set cursor to not be visible
+        Cursor.visible = false;
+	}
+}
